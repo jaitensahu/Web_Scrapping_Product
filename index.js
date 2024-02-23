fc570bd5feb20b0e27ffa29538e349e8acac2745
Jaiten <Jaitensahu9@gmail.com>
2024-02-24 00:58:07 +0530

    updated

--- index.js
+++ index.js
@@ -7,79 +7,57 @@ const workbook = xlsx.utils.book_new();
 // const { data } = require("cheerio/lib/api/attributes");
 let $;
 var titleArray = [];
-var cityArray = [];
-var postDate = [];
+var priceArray = [];
+var ratingArr = [];
 var jobTypeArr = [];
 var companyArray = [];
 async function scrapping() {
   await axios
-    .get("https://www.amazon.in/s?k=smart+watches&ref=nb_sb_noss_1")
+    .get("https://www.etsy.com/in-en/search?q=shirts&ref=search_bar")
     .then((res) => {
-      console.log(res.data);
+      // console.log(res.data);
       $ = cheerio.load(res.data);
-      $(".title-recipe").each((idx, ele) => {
-        // console.log($(ele).text());
+      $(".v2-listing-card__title").each((idx, element) => {
+        let title = $(element).text();
+        console.log(title);
+        titleArray.push(title);
       });
 
-      //   console.log(data);
-      //   // Job Title
-      //   $(".s-item__title").each((idx, element) => {
-      //     const title = $(element).text();
-      //     console.log(title);
-      //     // titleArray.push(title);
-      //   });
-      //   // Job Type
-      //   $(".m-salary .attributeSection .attributeVal").each((idx, element) => {
-      //     const jobType = $(element).text();
-      //     jobTypeArr.push(jobType);
-      //   });
+      //   //    Price
+      $(".currency-value").each((idx, element) => {
+        const price = $(element).text();
+        priceArray.push(price);
+      });
 
-      //   //    City
-      //   $(".city").each((idx, element) => {
-      //     const city = $(element).text();
-      //     cityArray.push(city);
-      //   });
-
-      //   //  Date of Post
-      //   $(".jsPostedOn").each((idx, element) => {
-      //     const date = $(element).text();
-
-      //     postDate.push(date);
-      //   });
+      // Rating
+    //   $(".stars-smaller").each((idx, element) => {
+    //     const rating = $(element).attr("aria-label");
+    //     console.log(rating);
+    //     //   ratingArr.push(rating);
+    //   });
       //   // Company name
-      //   $(".salary .attributeSection .attributeVal").each((idx, element) => {
-      //     const company = $(element).text();
-      //     if (idx % 2 != 0) {
-      //       companyArray.push(company);
-      //     }
-      //   });
+      console.log($(".streamline-seller-shop-name__line-height").children);
+        $(".streamline-seller-shop-name__line-height").each((idx, element) => {
+            console.log($(element).children(":eq(2)").text());
+            // const company = $(element).text();
+            // console.log(company);
+            companyArray.push($(element).children(":eq(2)").text());
+      });
 
-      //   const dataArr = [];
-      //   for (let i = 0; i < titleArray.length; i++) {
-      //     const tempArr = [
-      //       titleArray[i],
-      //       companyArray[i],
-      //       cityArray[i],
-      //       jobTypeArr[i],
-      //       postDate[i],
-      //     ];
-      //     dataArr.push(tempArr);
-      //   }
+      const dataArr = [];
+      for (let i = 0; i < titleArray.length; i++) {
+        const tempArr = [titleArray[i], priceArray[i], companyArray[i]];
+        dataArr.push(tempArr);
+      }
 
-      //   const sheetData = [
-      //     [
-      //       "Job Title",
-      //       "Company Name",
-      //       "Location",
-      //       "Job Type (Full-time, Part-time, Contract, etc.)",
-      //       "Posted Date",
-      //     ],
-      //     ...dataArr,
-      //   ];
-      //   const sheet = xlsx.utils.aoa_to_sheet(sheetData);
-      //   xlsx.utils.book_append_sheet(workbook, sheet, "Sheet1");
-      //   xlsx.writeFile(workbook, "output.xlsx");
-      //   console.log("XLSX file created successfully!");
+      const sheetData = [
+        ["Product Title", "Price", "Company Name"],
+        ...dataArr,
+      ];
+      const sheet = xlsx.utils.aoa_to_sheet(sheetData);
+      xlsx.utils.book_append_sheet(workbook, sheet, "Sheet1");
+      xlsx.writeFile(workbook, "output.xlsx");
+      console.log("XLSX file created successfully!");
     })
 
     .catch((err) => {
